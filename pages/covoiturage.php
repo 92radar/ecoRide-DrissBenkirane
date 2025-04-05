ed7bbb63a7efc804564c8968aeb580d72cd54627
92radar <viepro.db@gmail.com>
2025-04-05 15:55:10 +0200

    supression du filtre sql pour les filtre

--- pages/covoiturage.php
+++ pages/covoiturage.php
@@ -93,7 +93,6 @@ if (isset($_GET['depart']) && isset($_GET['arrivee']) && isset($_GET['date'])) {
                 $researchStmt->execute();
                 $researcheResult = $researchStmt->fetchAll(PDO::FETCH_OBJ);
                 $success = 'Recherche effectuée avec succès. ';
-                var_dump($researcheResult);
             } else {
                 $error = 'Aucun covoiturage trouvé';
                 $researcheResult = [];
@@ -110,12 +109,6 @@ if (isset($_POST['search'])) {
     $depart = $_POST['depart'];
     $arrivee = $_POST['arrivee'];
     $date = $_POST['date'];
-
-
-
-
-
-
     try {
         $researchStmt = $pdo->prepare("SELECT COUNT(*) FROM covoiturages WHERE lieu_depart = :lieu_depart AND lieu_arrivee = :lieu_arrivee AND date_depart =
 :date_depart AND nb_place > 0 ");
@@ -156,68 +149,8 @@ if (isset($_POST['search'])) {
     }
 }
 
-if (isset($_POST['applyFilters'])) {
 
 
-
-    $researcheResult = [];
-
-    // Filtrage des énergies disponibles
-    $energies = [];
-    if (!empty($_POST['Electrique'])) $energies[] = 'Electrique';
-    if (!empty($_POST['Hybride'])) $energies[] = 'Hybride';
-    if (!empty($_POST['Essence'])) $energies[] = 'Essence';
-
-    $prixmini = $_POST['prixmini'] ?? 0;
-    $prixmaxi = $_POST['prixmaxi'] ?? 999999;
-    $dureeMax = $_POST['dureeMax'] ?? null;
-    $evaluation = $_POST['evaluation'] ?? null;
-
-    // Début de la requête avec un JOIN
-    $sql = "
-        SELECT COUNT(*) 
-        FROM covoiturages 
-        JOIN voitures ON covoiturages.voiture_id = voitures.voiture_id
-        WHERE covoiturages.prix_personne BETWEEN :prixmini AND :prixmaxi
-    ";
-
-    // Ajout du filtre énergie si nécessaire
-    if (!empty($energies)) {
-        $placeholders = implode(", ", array_fill(0, count($energies), "?")); // ?, ?, ?
-        $sql .= " OR voitures.energie IN ($placeholders)";
-    }
-
-
-
-    if (!empty($evaluation)) {
-        $sql .= " OR covoiturages.evaluation >= :evaluation";
-    }
-
-    $researchStmt = $pdo->prepare($sql);
-
-    // Liaison des paramètres
-    $params = [':prixmini' => $prixmini, ':prixmaxi' => $prixmaxi];
-
-
-
-    if (!empty($evaluation)) {
-        $params[':evaluation'] = $evaluation;
-    }
-
-    // Ajout des valeurs des énergies
-    foreach ($energies as $index => $energie) {
-        $params[$index + 1] = $energie; // Les placeholders "?" sont positionnels
-    }
-
-    // Exécution de la requête
-
-    $researchStmt->execute(array_values($params));
-    $filterNumber = $researchStmt->fetch(PDO::FETCH_ASSOC);
-    $count = $filterNumber['COUNT(*)'];
-
-    $countSuccess = 'Nombre de covoiturages trouvés : ' . $count;
-    $success = 'Filtre appliqué avec succès.';
-}
 
 if (isset($_POST['participer'])) {
 
@@ -233,147 +166,154 @@ require_once '/Users/macosdev/Documents/GitHub/ecoRide-DrissBenkirane/elements/h
 
 
 
+<body>
+    <div style="display: flex;">
+        <div class="sidebar">
+            <p>Filtrer</p>
 
-<div style="display: flex;">
-    <div class="sidebar">
-        <p>Filtrer</p>
-
-        </br>
+            </br>
 
 
-        <div class="filter-container">
-            <form action="" method="post">
-                <div class="filter-group">
-                    <label>Type de voiture</label></br>
-                    <input class="form-check-input" type="checkbox" name="Electrique" value="Electrique"
-                        id="moteurElectrique">
-                    <label class="form-check-label" for="moteurElectrique">
-                        Electrique
-                    </label></br>
-                    <input class="form-check-input" type="checkbox" name="Hybride" value="Hybride" id="moteurHybride">
-                    <label class="form-check-label" for="moteurHybride">
-                        Hybride
-                    </label></br>
-                    <input class="form-check-input" type="checkbox" name="Essence" value="Essence" id="moteurEssence">
-                    <label class="form-check-label" for="moteurEssence">
-                        Essence
-                    </label></br>
-                </div>
-
-                <div class="filter-group">
-                    <label>Echelle de prix :</label> </br>
-                    <div class="price-range">
-                        <label for="prixMini">Minimum :</label>
-                        <input type="number" id="prixmini" name="prixmini" placeholder="minimum">
-                        <br>
-                        <label for="prixMaxi">Maximum :</label>
-                        <input type="number" id="prixmaxi" name="prixmaxi" placeholder="maximum">
+            <div class="filter-container">
+                <form action="" method="post">
+                    <div class="filter-group">
+                        <label>Type de voiture</label></br>
+                        <input class="form-check-input" type="checkbox" name="Electrique" value="Electrique"
+                            id="moteurElectrique">
+                        <label class="form-check-label" for="moteurElectrique">
+                            Electrique
+                        </label></br>
+                        <input class="form-check-input" type="checkbox" name="Hybride" value="Hybride"
+                            id="moteurHybride">
+                        <label class="form-check-label" for="moteurHybride">
+                            Hybride
+                        </label></br>
+                        <input class="form-check-input" type="checkbox" name="Essence" value="Essence"
+                            id="moteurEssence">
+                        <label class="form-check-label" for="moteurEssence">
+                            Essence
+                        </label></br>
                     </div>
-                </div>
 
-                <div class="filter-group">
-                    <label>Durée du voyage :</label></br>
-                    <div class="duration-filter">
-                        <label for="dureeMax">Max (ex: 1h30):</label>
-                        <input type="text" id="dureeMax" name="dureeMax" placeholder="max">
-                    </div>
-                </div>
-
-                <div class="filter-group">
-                    <label>Evaluations :</label></br>
-                    <?php for ($i = 5; $i >= 1; $i--) : ?>
-                        <div class="rating-filter">
-                            <input class="form-check-input" name="evaluation" type="checkbox" value="<?= $i ?>"
-                                id="rating<?= $i ?>">
-                            <?php for ($j = 0; $j < $i; $j++) : ?>
-                                <span class="star">⭐</span>
-                            <?php endfor; ?>
-                            <?php for ($j = $i; $j < 5; $j++) : ?>
-                                <span class="star">☆</span>
-                            <?php endfor; ?>
+                    <div class="filter-group">
+                        <label>Echelle de prix :</label> </br>
+                        <div class="price-range">
+                            <label for="prixMini">Minimum :</label>
+                            <input type="number" id="prixmini" name="prixmini" placeholder="minimum">
+                            <br>
+                            <label for="prixMaxi">Maximum :</label>
+                            <input type="number" id="prixmaxi" name="prixmaxi" placeholder="maximum">
                         </div>
-                    <?php endfor; ?>
-                </div>
-                <button id="applyFiltersBtn" type="submit" name="applyFilters" class="apply-filters-button">Appliquer
-                    les
-                    filtres</button></br>
-            </form>
+                    </div>
+
+                    <div class="filter-group">
+                        <label>Durée du voyage :</label></br>
+                        <div class="duration-filter">
+                            <label for="dureeMax">Max (ex: 1h30):</label>
+                            <input type="text" id="dureeMax" name="dureeMax" placeholder="max">
+                        </div>
+                    </div>
+
+                    <div class="filter-group">
+                        <label>Evaluations :</label></br>
+                        <?php for ($i = 5; $i >= 1; $i--) : ?>
+                            <div class="rating-filter">
+                                <input class="form-check-input" name="evaluation" type="checkbox" value="<?= $i ?>"
+                                    id="rating<?= $i ?>">
+                                <?php for ($j = 0; $j < $i; $j++) : ?>
+                                    <span class="star">⭐</span>
+                                <?php endfor; ?>
+                                <?php for ($j = $i; $j < 5; $j++) : ?>
+                                    <span class="star">☆</span>
+                                <?php endfor; ?>
+                            </div>
+                        <?php endfor; ?>
+                    </div>
+                    <button id="applyFiltersBtn" type="submit" name="applyFilters"
+                        class="apply-filters-button">Appliquer
+                        les
+                        filtres</button></br>
+                </form>
+            </div>
         </div>
-    </div>
 
-    <div style="flex-grow: 1;">
-        <div class="ligne-horizontale"></div></br>
-        <?php if (isset($error)) : ?>
-            <div class="alert alert-danger" role="alert">
-                <?= $error ?>
-            </div>
-        <?php endif; ?>
-        <?php if (isset($success)) : ?>
-            <div class="alert alert-success container" role="alert">
-                <?= $success ?></br>
-                <?= $countSuccess ?>
-            </div>
-        <?php endif; ?></br>
-
-        <?php foreach ($researcheResult as $result): ?>
-            <form method="post">
-                <div class="publication-cadre">
-                    <div class="publication-header">
-                        <div class="utilisateur-info">
-                            <span>
-                                <img src="<?= $result->photo ?>" alt="Photo de <?= htmlspecialchars($result->nom) ?>"
-                                    class="photo-utilisateur" height="50" width="50">
-                            </span>
-                            <span class="utilisateur" name="nom">
-                                <?= htmlspecialchars($result->nom) ?></br><?= htmlspecialchars($result->prenom) ?>
-                            </span>
-                        </div>
-                        <span class="date-creation" name="created_at">
-                            **Publié le : <?= htmlspecialchars($result->created_at) ?>**
-                        </span>
-                    </div>
-
-                    <div class="publication-details">
-                        <div class="trajet">
-                            <h3>Trajet</h3>
-                            <p>
-                                <strong>Départ :</strong> <?= htmlspecialchars($result->lieu_depart) ?>
-                                <br>
-                                <strong>Arrivée :</strong> <?= htmlspecialchars($result->lieu_arrivee) ?>
-                            </p>
-                        </div>
-
-                        <div class="dates">
-                            <h3>Dates et Horaires</h3>
-                            <p>
-                                <strong>Départ :</strong>
-                                <?= htmlspecialchars(date('d/m/Y', strtotime($result->date_depart))) ?> à
-                                <?= htmlspecialchars(date('H:i', strtotime($result->heure_depart))) ?> h
-                                <br>
-                                <strong>Arrivée :</strong>
-                                <?= htmlspecialchars(date('d/m/Y', strtotime($result->date_arrivee))) ?> à
-                                <?= htmlspecialchars(date('H:i', strtotime($result->heure_arrivee))) ?> h
-                            </p>
-                        </div>
-
-                        <div class="informations">
-                            <h3>Informations</h3>
-                            <p>
-                                <strong>Type de voiture :</strong> <?= htmlspecialchars($result->energie) ?>
-                                <br>
-                                <strong>Places disponibles :</strong> <?= htmlspecialchars($result->nb_place) ?>
-                                <br>
-                                <strong>Prix par place :</strong> <?= htmlspecialchars($result->prix_personne) ?> Credits
-                            </p>
-                        </div>
-
-                        <div class="publication-actions">
-                            <input type="hidden" name="covoiturage_id"
-                                value="<?= htmlspecialchars($result->covoiturage_id) ?>">
-                            <button class="participer-btn" type="submit" name="participer">Participer</button>
-                        </div>
-                    </div>
+        <div style="flex-grow: 1;">
+            <div class="ligne-horizontale"></div></br>
+            <?php if (isset($error)) : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $error ?>
                 </div>
-            </form>
-        <?php endforeach; ?>
-    </div>
+            <?php endif; ?>
+            <?php if (isset($success)) : ?>
+                <div class="alert alert-success container" role="alert">
+                    <?= $success ?></br>
+                    <?= $countSuccess ?>
+                </div>
+            <?php endif; ?></br>
+
+            <?php foreach ($researcheResult as $result): ?>
+                <form method="post">
+                    <div class="publication-cadre">
+                        <div class="publication-header">
+                            <div class="utilisateur-info">
+                                <span>
+                                    <img src="<?= $result->photo ?>" alt="Photo de <?= htmlspecialchars($result->nom) ?>"
+                                        class="photo-utilisateur" height="50" width="50">
+                                </span>
+                                <span class="utilisateur" name="nom">
+                                    <?= htmlspecialchars($result->nom) ?></br><?= htmlspecialchars($result->prenom) ?>
+                                </span>
+                            </div>
+                            <span class="date-creation" name="created_at">
+                                **Publié le : <?= htmlspecialchars($result->created_at) ?>**
+                            </span>
+                        </div>
+
+                        <div class="publication-details">
+                            <div class="trajet">
+                                <h3>Trajet</h3>
+                                <p>
+                                    <strong>Départ :</strong> <?= htmlspecialchars($result->lieu_depart) ?>
+                                    <br>
+                                    <strong>Arrivée :</strong> <?= htmlspecialchars($result->lieu_arrivee) ?>
+                                </p>
+                            </div>
+
+                            <div class="dates">
+                                <h3>Dates et Horaires</h3>
+                                <p>
+                                    <strong>Départ :</strong>
+                                    <?= htmlspecialchars(date('d/m/Y', strtotime($result->date_depart))) ?> à
+                                    <?= htmlspecialchars(date('H:i', strtotime($result->heure_depart))) ?> h
+                                    <br>
+                                    <strong>Arrivée :</strong>
+                                    <?= htmlspecialchars(date('d/m/Y', strtotime($result->date_arrivee))) ?> à
+                                    <?= htmlspecialchars(date('H:i', strtotime($result->heure_arrivee))) ?> h
+                                </p>
+                            </div>
+
+                            <div class="informations">
+                                <h3>Informations</h3>
+                                <p>
+                                    <strong class="energie">Type de voiture :</strong>
+                                    <?= htmlspecialchars($result->energie) ?>
+                                    <br>
+                                    <strong>Places disponibles :</strong> <?= htmlspecialchars($result->nb_place) ?>
+                                    <br>
+                                    <strong class="prix">Prix par place :</strong>
+                                    <?= htmlspecialchars($result->prix_personne) ?> Credits
+                                </p>
+                            </div>
+
+                            <div class="publication-actions">
+                                <input type="hidden" name="covoiturage_id"
+                                    value="<?= htmlspecialchars($result->covoiturage_id) ?>">
+                                <button class="participer-btn" type="submit" name="participer">Participer</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            <?php endforeach; ?>
+        </div>
+        <script src="/JS/filter_script.js"></script>
+</body>
